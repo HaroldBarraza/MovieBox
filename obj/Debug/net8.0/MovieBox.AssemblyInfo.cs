8ba403a5593f6ea3dd308d912caf10b72d53fd13
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-10-10 13:19:09 -0500

    blazor webapp

--- obj/Debug/net8.0/MovieBox.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieBox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409a112f97f4bc40dc62671ede98402464de4a2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fcb6a8c051d6c53511146614812a02d0b215c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
