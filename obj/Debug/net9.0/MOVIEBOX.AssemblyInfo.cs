ffa3976dae9b9b33e040aabe313097d9741e4d1b
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-10-10 18:34:19 -0500

    update 9

--- obj/Debug/net9.0/MOVIEBOX.AssemblyInfo.cs
+++ obj/Debug/net9.0/MOVIEBOX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f6c49ed540721a7da019db8cd2cd3b84770f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MOVIEBOX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
